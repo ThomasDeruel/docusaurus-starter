257ea3e1ab9c7a260e368d2a572eb59d2447bb5f
thomas deruel <thomas.deruel@hetic.net>
2021-04-11 15:47:31 +0200

    edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,13 +85,13 @@ module.exports = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            "https://github.com/ThomasDeruel/docusaurus-starter/edit/master/website/",
+            "https://github.com/ThomasDeruel/sup-doc/edit/master/website/",
         },
         // blog: {
         //   showReadingTime: true,
         //   // Please change this to your repo.
         //   editUrl:
-        //     "https://github.com/ThomasDeruel/docusaurus-starter/edit/master/website/blog/",
+        //     "https://github.com/ThomasDeruel/sup-doc/edit/master/website/blog/",
         // },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
