10a401de48794e11b44724342239f0db800df3eb
thomas deruel <thomas.deruel@hetic.net>
2021-04-11 15:42:25 +0200

    fix blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -63,10 +63,10 @@ module.exports = {
         {
           title: "More",
           items: [
-            {
-              label: "Blog",
-              to: "blog",
-            },
+            // {
+            //   label: "Blog",
+            //   to: "blog",
+            // },
             {
               label: "GitHub",
               href: "https://github.com/facebook/docusaurus",
